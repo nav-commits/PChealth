9277b807d95c6c57d4d43ea3733ec320de2427b4
Nav <navdeep.dhamrait@thebioagency.com>
2023-09-05 15:57:47 -0400

    fixed issues

--- src/Components/Screens/TabScreens/Pharmacy.jsx
+++ src/Components/Screens/TabScreens/Pharmacy.jsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function Pharmacy() {
     return (
-        <View style={{ padding: 40 }}>
-            <Text>Pharmacy</Text>
+        <View style={{ paddingLeft: 20, paddingTop: 50 }}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Pharmacy Page</Text>
         </View>
     );
 }
